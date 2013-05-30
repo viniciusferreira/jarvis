37b1118f425c635d9c744b8a8f250d4aee3cd335
Martin Polden <martin.polden@gmail.com>
2013-05-30 20:27:31 +0200

    yr: Show observed temperature (usually more accurate)

--- jobs/yr.rb
+++ jobs/yr.rb
@@ -7,17 +7,18 @@ YR_CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml'))
 SCHEDULER.every '15m', :first_in => 0 do |job|
   doc = Nokogiri::XML(open(YR_CONFIG['yr']['url']))
   tabular = doc.xpath('/weatherdata/forecast/tabular/time[1]')
-  windSpeed = tabular.xpath('windSpeed')
+  weatherStation = doc.xpath('/weatherdata/observations/weatherstation[1]')
+  windSpeed = weatherStation.xpath('windSpeed')
   data = {
     'location' => doc.xpath('/weatherdata/location/name').text,
     'from' => tabular.attr('from').value,
     'to' => tabular.attr('to').value,
-    'temperature' => tabular.xpath('temperature').attr('value').value,
+    'temperature' => weatherStation.xpath('temperature').attr('value').value,
     'description' => tabular.xpath('symbol').attr('name').value,
     'wind' => {
       'speed' => windSpeed.attr('mps').value,
       'description' => windSpeed.attr('name').value,
-      'direction' => tabular.xpath('windDirection').attr('name').value
+      'direction' => weatherStation.xpath('windDirection').attr('name').value
     }
   }
   send_event('yr', data)
